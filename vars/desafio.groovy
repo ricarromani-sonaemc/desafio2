e5a42a62bbb83e1d70d965f1cc8bec7c627075db
ricarromani-sonaemc <ricarromani@parceiro.sonae.pt>
2022-07-05 17:44:46 +0100

    Update desafio.groovy

--- vars/desafio.groovy
+++ vars/desafio.groovy
@@ -1,16 +1,19 @@
 def call(body) {
+
     pipeline {
         agent any
         stages {
             stage('readYaml') {
                 steps {
-                    script {
-                        ymlObj = readYaml file: "yaml-families/family.yaml" 
-                        echo "${ymlObj}"
-                        echo "${ymlObj.getClass()}"
+                    switch(env.BRANCH_NAME) {
+                        case 'yaml':
+                            ymlObj = readYaml file: "yaml-families/family.yaml" 
+                            echo "${ymlObj}"
+                            echo "${ymlObj.getClass()}"
+                            break
                     }
                 }
             }
         }
     }
-} 
+}
