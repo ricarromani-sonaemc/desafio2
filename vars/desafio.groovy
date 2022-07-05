b4d23f15800b6392d79300f057e9e57e8aa3fea9
ricarromani-sonaemc <ricarromani@parceiro.sonae.pt>
2022-07-05 17:59:02 +0100

    Update desafio.groovy

--- vars/desafio.groovy
+++ vars/desafio.groovy
@@ -1,20 +1,25 @@
 def call(body) {
 
+    def branch = env.BRANCH_NAME 
+
     pipeline {
         agent any
         stages {
             stage('readYaml') {
                 steps {
+                    echo "$branch"
+                    /*
                     switch(env.BRANCH_NAME) {
                         case 'yaml':
                             ymlObj = readYaml file: "yaml-families/family.yaml" 
                             echo "${ymlObj}"
                             echo "${ymlObj.getClass()}"
                             break
-                            
+
                         default:
                         error('Unexpected branch name')
                     }
+                    */
                 }
             }
         }
