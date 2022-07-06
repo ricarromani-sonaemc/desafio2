15e80cd29dd79001de28544cb0aa86b9b9cc355b
ricarromani-sonaemc <ricarromani@parceiro.sonae.pt>
2022-07-06 15:28:02 +0100

    Update desafio.groovy

--- vars/desafio.groovy
+++ vars/desafio.groovy
@@ -12,10 +12,10 @@ def call(body) {
                     script {
                     git branch: "yaml", credentialsId: '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', url: "https://github.com/ricarromani-sonaemc/desafio2.git"
                     
-                    echo "${WORKSPACE}"
+                    sh "ls ${WORKSPACE}"
                     yamlObj = readYaml file: "${WORKSPACE}/yaml-families/family.yaml"
                     //yamlObj = readYaml(file: "${WORKSPACE}\\yaml-families\\family.yaml")
-                    //echo "${ymlObj}"
+                    echo "${ymlObj}"
                     //echo "${ymlObj.getClass()}"                        
                     }
 
