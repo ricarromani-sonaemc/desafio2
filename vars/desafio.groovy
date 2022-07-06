f1f90b30c4013264a2bcc113f9b27fd09cac8bde
ricarromani-sonaemc <ricarromani@parceiro.sonae.pt>
2022-07-06 11:46:33 +0100

    1

--- vars/desafio.groovy
+++ vars/desafio.groovy
@@ -8,13 +8,13 @@ def call(body) {
         stages {
             stage('init') {
                 steps {
-                    git branch: "yaml", credentialsId: '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', url: "https://github.com/ricarromani-sonaemc/desafio2.git"
+                    //git branch: "yaml", credentialsId: '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', url: "https://github.com/ricarromani-sonaemc/desafio2.git"
                 }
             }
 
             stage('readYaml') {
                 steps {
-                    ymlObj = readYaml(file: "/var/jenkins_home/workspace/aqms-desafio2/yaml-families/family.yaml")
+                    ymlObj = readYaml(file: "yaml-families/family.yaml")
                     //echo "${WORKSPACE}" // /var/jenkins_home/workspace/aqms-desafio2
                     //ymlObj = readYaml file: "yaml-families/family.yaml"
                     echo "${ymlObj}"
