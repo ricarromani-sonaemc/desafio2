35cf20f53e4363c94326ce767ac1cfe748438916
ricarromani-sonaemc <ricarromani@parceiro.sonae.pt>
2022-07-08 11:28:28 +0100

    Update desafio.groovy

--- vars/desafio.groovy
+++ vars/desafio.groovy
@@ -50,8 +50,10 @@ def call(body) {
                             echo "A familia ${yamlObj.families[i].family[0].name} é constituida por ${yamlObj.families[i].family[0].members.size()} membros."
 
                             for(int f = 0; f < yamlObj.families[i].family[0].members.size(); f++) {
+                                77yamlObj.families[i].family[0].members[f].lastName
                                 
                                 
+                                echo "${yamlObj.families[i].family[0].members[f].lastName}"
                                 echo "${yamlObj.families[i].family[0].members[f].lastName}"
                                 echo "${families}"
 
